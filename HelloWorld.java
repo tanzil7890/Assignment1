7933a8a221dd93f107da040ae923365c36a79b42
Mohammad Tanzil Idrisi <tanzilidrisi@Mohammads-MacBook-Air.local>
2024-09-13 03:48:31 -0500

    Modified Name

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, World! My name is Abhas Oli. I have been at Beloit for 2 years");
+        System.out.println("Hello, World! My name is Tanzil");
     }
 }
